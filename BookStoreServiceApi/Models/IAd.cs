1c200358e9f842e665132b1ceba09270fd83a92f
Streebor <stefanvasiljevic@me.com>
2015-11-07 17:38:04 -0500

    API Interface

---
+++ BookStoreServiceApi/Models/IAd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreServiceApi.Models
+{
+    public interface IAd
+    {
+        /// <summary>
+        /// Unique ID of an Ad in DB.
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Title of an Ad.
+        /// </summary>
+        string Title { get; set; }
+
+        /// <summary>
+        /// Description of an Ad.
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Book being sold.
+        /// </summary>
+        IBook Book { get; set; }
+
+        /// <summary>
+        /// Asking Price for a Book being sold in this Ad.
+        /// </summary>
+        decimal Price { get; set; }
+
+        /// <summary>
+        /// User that posted Ad.
+        /// </summary>
+        IUser UserPosted { get; set; }
+
+        /// <summary>
+        /// Date and time that Ad has been posted on.
+        /// </summary>
+        DateTime PostTimestamp { get; set; }
+
+        /// <summary>
+        /// Tags for this Ad.
+        /// </summary>
+        IList<string> Tags { get; set; }
+    }
+}
---
+++ BookStoreServiceApi/Models/IAuthor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreServiceApi.Models
+{
+    public interface IAuthor
+    {
+        /// <summary>
+        /// Unique ID of Author in DB.
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Author's full name. 
+        /// </summary>
+        string FullName { get; set; }
+    }
+}
